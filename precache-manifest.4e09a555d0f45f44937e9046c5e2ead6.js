38966b020754683d40481ed1c3b42a49f54c7a02
John Cambi <jcambi@myseneca.ca>
2019-07-02 13:34:52 -0400

    Updates

--- precache-manifest.4e09a555d0f45f44937e9046c5e2ead6.js
+++ precache-manifest.4e09a555d0f45f44937e9046c5e2ead6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ce6961446c7960e874ddad2d0885d9b",
+    "revision": "b46e91cba4f0fd263bbd3eb0172c5449",
     "url": "/WEB422-A4/index.html"
   },
   {
-    "revision": "27bc28fe21a22616d295",
+    "revision": "7384cd55e8d193a42218",
     "url": "/WEB422-A4/static/css/main.856419da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/WEB422-A4/static/js/2.fd47fb84.chunk.js"
   },
   {
-    "revision": "27bc28fe21a22616d295",
-    "url": "/WEB422-A4/static/js/main.d0dc258f.chunk.js"
+    "revision": "7384cd55e8d193a42218",
+    "url": "/WEB422-A4/static/js/main.da0ee040.chunk.js"
   },
   {
     "revision": "ae3624ddc6aed0edb399",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WEB422-A4/precache-manifest.012f7278dccd63112b2b3f15712764ad.js"
+  "/WEB422-A4/precache-manifest.4e09a555d0f45f44937e9046c5e2ead6.js"
 );
 
 self.addEventListener('message', (event) => {
