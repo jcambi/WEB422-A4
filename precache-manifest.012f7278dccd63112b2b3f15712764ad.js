988cbb75d0cf816d8f62d3c4a7b3993b8d966690
John Cambi <jcambi@myseneca.ca>
2019-07-02 13:23:29 -0400

    Updates

---
+++ precache-manifest.012f7278dccd63112b2b3f15712764ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce6961446c7960e874ddad2d0885d9b",
+    "url": "/WEB422-A4/index.html"
+  },
+  {
+    "revision": "27bc28fe21a22616d295",
+    "url": "/WEB422-A4/static/css/main.856419da.chunk.css"
+  },
+  {
+    "revision": "e1f61f96a2ae42694db0",
+    "url": "/WEB422-A4/static/js/2.fd47fb84.chunk.js"
+  },
+  {
+    "revision": "27bc28fe21a22616d295",
+    "url": "/WEB422-A4/static/js/main.d0dc258f.chunk.js"
+  },
+  {
+    "revision": "ae3624ddc6aed0edb399",
+    "url": "/WEB422-A4/static/js/runtime~main.98ac1aa3.js"
+  }
+]);
